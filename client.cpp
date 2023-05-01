e93a7fb4c1618a24eda709cdbfbbaa77d09096b1
Mihail Borovkov <1mihails.b@gmail.com>
2023-05-01 19:06:09 +0300

    epoll example server working with multiple clients
    - start ./server
    - start multiple ./client localhost

--- client.cpp
+++ client.cpp
@@ -1,57 +1,88 @@
-//
-// client.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
-#include <iostream>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
+#define PORT "3490" // the port client will be connecting to 
 
-using boost::asio::ip::tcp;
-
-int main(int argc, char* argv[])
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-  try
-  {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: client <host>" << std::endl;
-      return 1;
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
     }
 
-    boost::asio::io_context io_context;
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
-    tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints =
-      resolver.resolve(argv[1], "daytime");
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
 
-    tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints);
-
-    for (;;)
-    {
-      boost::array<char, 128> buf;
-      boost::system::error_code error;
-
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
-
-      std::cout.write(buf.data(), len);
+    if (argc != 2) {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
     }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
 
-  return 0;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    int times = 0;
+    while(1) {
+        times++;
+        int sent = 0;
+        char text[17];
+        sprintf(text, "Hello%d", times);
+
+        if ((sent = send(sockfd, text, 17, 0)) == -1) {
+            perror("send");
+            exit(1);
+        }
+
+        sleep(2);
+    }
+
+    close(sockfd);
+
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,50 +1,146 @@
-//
-// server.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include <ctime>
 #include <iostream>
-#include <string>
-#include <boost/asio.hpp>
+#include <netdb.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/epoll.h>
 
-using boost::asio::ip::tcp;
-
-std::string make_daytime_string()
-{
-  using namespace std; // For time_t, time and ctime;
-  time_t now = time(0);
-  return ctime(&now);
-}
+#define PORT "3490"
+#define BACKLOG 10
 
 int main()
 {
-  try
-  {
-    boost::asio::io_context io_context;
+    int listenSockfd, new_fd;
+    struct addrinfo hints, *servinfo, *p;
+    int yes = 1;
+    int rv;
 
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((listenSockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(listenSockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(listenSockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(listenSockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(listenSockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    // epoll
+
+    struct epoll_event ev, events[BACKLOG];
+    int conn_sock, nfds, epollfd;
+
+    epollfd = epoll_create1(0);
+    if (epollfd == -1)
+    {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    ev.events = EPOLLIN;
+    ev.data.fd = listenSockfd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listenSockfd, &ev) == -1)
+    {
+        perror("epoll_ctl: listenSockfd");
+        exit(EXIT_FAILURE);
+    }
+
+    const size_t bufs = 4096;
+    int curSize = 0;
+    char buf[bufs];
 
     for (;;)
     {
-      tcp::socket socket(io_context);
-      acceptor.accept(socket);
+        nfds = epoll_wait(epollfd, events, BACKLOG, -1);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
 
-      std::string message = make_daytime_string();
+        for (int n = 0; n < nfds; ++n)
+        {
+            if (events[n].data.fd == listenSockfd)
+            {
+                sockaddr con_address;
+                socklen_t casize = sizeof con_address;
+                conn_sock = accept(listenSockfd, &con_address, &casize);
+                if (conn_sock == -1)
+                {
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
 
-      boost::system::error_code ignored_error;
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+                if (setsockopt(conn_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+                {
+                    perror("setsockopt conn_sock");
+                    exit(1);
+                }
+
+                ev.events = EPOLLIN; // EPOLLIN | EPOLLET
+                ev.data.fd = conn_sock;
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
+                              &ev) == -1)
+                {
+                    perror("epoll_ctl: conn_sock");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                curSize = recv(events[n].data.fd, &buf, bufs, 0);
+                if (curSize == -1) {
+                    perror("recv:");
+                } else if (curSize == 0) {
+                    if (epoll_ctl(epollfd, EPOLL_CTL_DEL, events[n].data.fd, &events[n]) == -1) {
+                        perror("epoll_ctl EPOLL_CTL_DEL:");
+                    }
+                }
+
+                buf[curSize] = '\0';
+                printf("received from client: %s\n", buf);
+                curSize = 0;
+                memset(&buf, 0, bufs);
+            }
+        }
     }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
 
-  return 0;
+    return 0;
 }
