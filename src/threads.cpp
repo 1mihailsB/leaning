f5b7f322d2c24d23dc8831bf9414d2a1fcc82c52
Mihail Borovkov <1mihails.b@gmail.com>
2023-06-10 22:05:57 +0300

    use thread condition variable to syncrhonize consumers and producer

--- src/threads.cpp
+++ src/threads.cpp
@@ -5,47 +5,64 @@
 #include <unistd.h>
 #include "queue.cpp"
 
-#define THREADS 1000
+#define THREADS 10
+#define WORKSIZE 1000
 
 static pthread_t threads[THREADS];
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t lock;
 static int count = 0;
+static bool workfinished = false;
+
+Queue jq = Queue<float>::init(1000);
 
 void *work(void *arg) {
     pthread_t *t = (pthread_t*) arg;
     pthread_t tnum = *t;
 
-    int r = rand();
-    int s = 1 + r % 10;
-    // int s = 3;
+    for(;;) {
+        pthread_mutex_lock(&lock);
+        while(jq.size == 0) {
+            printf("Thread %lu waiting for condition\n", tnum);
+            pthread_cond_wait(&cond, &lock);
 
-    pthread_mutex_lock(&lock);
-    int c = count;
+            if (count >= WORKSIZE) {
+                printf("Thread %lu returning\n", tnum);
+                pthread_cond_broadcast(&cond);
+                pthread_mutex_unlock(&lock);
+                return NULL;
+            }
+        }
 
-    printf("Work function #%lu, thread #%lu, count: %d\n", tnum, threads[tnum], count);
-    printf("Sleeping: %d * 100000 microsec\n", s);
+        int c = count;
+        float item = jq.take();
 
-    count = c + 1;
-    pthread_mutex_unlock(&lock);
+        printf("Work function #%lu, thread #%lu, count: %d\n", tnum, threads[tnum], count);
+        printf("Queue size: %d, value: %.2f\n", jq.size, item);
 
-    usleep(s * 100000);
-    return NULL;
+        count = c + 1;
+        if (count >= WORKSIZE) {
+            pthread_cond_broadcast(&cond);
+        }
+        usleep(10000);
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+void initQueue() {
+    for (int i = 0; i < WORKSIZE; i++) {
+        pthread_mutex_lock(&lock);
+
+        jq.add(55.32);
+        pthread_cond_signal(&cond);
+
+        pthread_mutex_unlock(&lock);
+
+        // sleep(1);
+    }
 }
 
 int main() {
-    Queue jq = Queue<float>::init(1000);
-    for (int i = 0; i < 1000; i++) {
-        jq.add(55.32);
-    }
-
-
-    for (int i = 0; jq.size > 0; i++) {
-        float take = jq.take();
-        printf("Taken job:%.2f\n", take);
-    }
-
-    printf("Size of queue: %lu\n", sizeof(jq));
-
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
@@ -56,16 +73,17 @@ int main() {
         return 1;
     }
 
+    // initQueue();
     for (int i = 0; i < THREADS; i++) {
         list[i] = i;
         int num = pthread_create(&threads[i], &attr, work, (void*) (list + i));
-        // printf("%d created thread %ld\n", i, threads[i]);
     }
 
+    initQueue();
 
     for (int i = 0; i < THREADS; i++) {
+        printf("thread join %d\n", i);
         pthread_join(threads[i], NULL);
-        printf("Sleep %d\n", i);
     }
 
     printf("Count %d, \n", count);
